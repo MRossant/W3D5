19e5c4544d82f090773697727bb21722fc08ade5
Tristan Angeles <setswipe@gmail.com>
2021-08-27 13:55:44 -0400

    created initialize, working on parent=

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,22 @@
+require "byebug"
+
+
+
 class PolyTreeNode
 
-end
+  attr_reader :value, :parent, :children
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(node)
+    if self.parent !=node
+      self.parent = node
+    end
+
+  end
+
+end # end PolyTreeNode Class
